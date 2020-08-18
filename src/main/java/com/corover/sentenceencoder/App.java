dfa2037b690ad6a5f2b6f4b43a19e35016c53b77
rafiquemd <rafiq41cb@yahoo.com>
2020-08-18 22:06:29 +0530

    timezone updated

--- src/main/java/com/corover/sentenceencoder/App.java
+++ src/main/java/com/corover/sentenceencoder/App.java
@@ -3,8 +3,16 @@ package com.corover.sentenceencoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class App {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
